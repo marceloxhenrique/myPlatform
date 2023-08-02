db07e58a9fd74d352bb36e1d6e9105981ef0ae82
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-08-02 15:48:34 +0200

    Set up Navbar desktop

--- frontend/src/components/cards/course/CourseCard.jsx
+++ frontend/src/components/cards/course/CourseCard.jsx
@@ -1,31 +1,58 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import styles from "./CourseCard.module.css";
+
+// export default function cardCourse({ coursesAvailable }) {
+//   // console.log(coursesAvailable)
+//   return (
+//     <div>
+//       {coursesAvailable &&
+//         coursesAvailable.map((course) => (
+//           <div key={course.id} className={styles.cardCountainer}>
+//             <section
+//               style={{ backgroundColor: `${course.color}` }}
+//               className={styles.leftSide}
+//             >
+//               <p className={styles.initials}>{course.initials}</p>
+//             </section>
+//             <section className={styles.rightSide}>
+//               <h2>{course.title}</h2>
+//               <p>{course.description}</p>
+//               <Link to={`/course/${course.id}`}>
+//                 <div className={styles.buttonAllLessons}>
+//                   <button type="button">See all lessons</button>
+//                 </div>
+//               </Link>
+//             </section>
+//           </div>
+//         ))}
+//     </div>
+//   );
+// }
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./CourseCard.module.css";
 
-export default function cardCourse({ coursesAvailable }) {
-  // console.log(coursesAvailable)
+export default function cardCourse({ course }) {
   return (
     <div>
-      {coursesAvailable &&
-        coursesAvailable.map((course) => (
-          <div key={course.id} className={styles.cardCountainer}>
-            <section
-              style={{ backgroundColor: `${course.color}` }}
-              className={styles.leftSide}
-            >
-              <p className={styles.initials}>{course.initials}</p>
-            </section>
-            <section className={styles.rightSide}>
-              <h2>{course.title}</h2>
-              <p>{course.description}</p>
-              <Link to={`/course/${course.id}`}>
-                <div className={styles.buttonAllLessons}>
-                  <button type="button">See all lessons</button>
-                </div>
-              </Link>
-            </section>
-          </div>
-        ))}
+      <div key={course.id} className={styles.cardCountainer}>
+        <section
+          style={{ backgroundColor: `${course.color}` }}
+          className={styles.leftSide}
+        >
+          <p className={styles.initials}>{course.initials}</p>
+        </section>
+        <section className={styles.rightSide}>
+          <h2>{course.title}</h2>
+          <p>{course.description}</p>
+          <Link to={`/course/${course.id}`}>
+            <div className={styles.buttonAllLessons}>
+              <button type="button">See all lessons</button>
+            </div>
+          </Link>
+        </section>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Course/Course.jsx
+++ frontend/src/pages/Course/Course.jsx
@@ -1,3 +1,33 @@
+// import React, { useEffect, useState } from "react";
+
+// import styles from "./Course.module.css";
+// import { api } from "../../services/api";
+// import CourseCard from "../../components/cards/course/CourseCard";
+
+// export default function Course() {
+//   const [coursesAvailable, setCoursesAvailable] = useState();
+
+//   useEffect(() => {
+//     const getCourses = async () => {
+//       try {
+//         const resCoursesAvailables = await api.getAllCourses();
+//         setCoursesAvailable(resCoursesAvailables);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     getCourses();
+//   }, []);
+
+//   return (
+//     <div className={styles.containerCourse}>
+//       <main>
+//         {coursesAvailable && <CourseCard coursesAvailable={coursesAvailable} />}
+//       </main>
+//     </div>
+//   );
+// }
+
 import React, { useEffect, useState } from "react";
 
 import styles from "./Course.module.css";
@@ -11,7 +41,6 @@ export default function Course() {
     const getCourses = async () => {
       try {
         const resCoursesAvailables = await api.getAllCourses();
-
         setCoursesAvailable(resCoursesAvailables);
       } catch (error) {
         console.error(error);
@@ -23,7 +52,11 @@ export default function Course() {
   return (
     <div className={styles.containerCourse}>
       <main>
-        {coursesAvailable && <CourseCard coursesAvailable={coursesAvailable} />}
+        {/* {coursesAvailable && <CourseCard coursesAvailable={coursesAvailable} />} */}
+        {coursesAvailable &&
+          coursesAvailable.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
       </main>
     </div>
   );
