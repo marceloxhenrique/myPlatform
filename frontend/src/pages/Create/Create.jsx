d53094ebb9a44cbe53a94eec1c2c054696ea1039
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-08-08 16:42:15 +0200

    Fixed css in the Create.jsx

--- frontend/src/pages/Create/Create.jsx
+++ frontend/src/pages/Create/Create.jsx
@@ -6,17 +6,17 @@ import styles from "./Create.module.css";
 export default function Create() {
   return (
     <div className={styles.containerCreatecourse}>
-      <h1>Create </h1>
+      {/* <h1>Create </h1> */}
       {/* <CardcreateCourse /> */}
       <section className={styles.adminContainer}>
-        <Link to="/createcourse">
-          <div className={styles.create}>Create a new course</div>
+        <Link className={styles.create} to="/createcourse">
+          <div>Create a new course</div>
         </Link>
-        <Link to="/updatecourse">
-          <div className={styles.update}>Update an course</div>
+        <Link className={styles.update} to="/updatecourse">
+          <div>Update an course</div>
         </Link>
-        <Link to="/createlesson">
-          <div className={styles.create}>Create a new lesson</div>
+        <Link className={styles.createLesson} to="/createlesson">
+          <div>Create a new lesson</div>
         </Link>
       </section>
     </div>
