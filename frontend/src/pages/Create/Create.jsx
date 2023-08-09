661a398beaed3c92765b77cdef0e87e6c352175b
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-08-09 23:35:56 +0200

    Update icons in Create.jsx and update database.sql

--- frontend/src/pages/Create/Create.jsx
+++ frontend/src/pages/Create/Create.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import CardcreateCourse from "../../components/cards/createCourse/CardcreateCourse";
+import { BiEdit, BiPlusCircle } from "react-icons/bi";
 import styles from "./Create.module.css";
 
 export default function Create() {
   return (
     <div className={styles.containerCreatecourse}>
-      {/* <h1>Create </h1> */}
-      {/* <CardcreateCourse /> */}
       <section className={styles.adminContainer}>
         <Link className={styles.create} to="/createcourse">
+          <BiPlusCircle className={styles.icon} />
           <div>Create a new course</div>
         </Link>
         <Link className={styles.update} to="/updatecourse">
+          <BiEdit className={styles.icon} />
           <div>Update an course</div>
         </Link>
         <Link className={styles.createLesson} to="/createlesson">
+          <BiPlusCircle className={styles.icon} />
           <div>Create a new lesson</div>
         </Link>
       </section>
