02621aa7eec8cfdddd5c071e6fd02169ef8f07c6
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-08-21 23:20:34 +0200

    Added an refresh token to  auth.js

--- backend/src/services/auth.js
+++ backend/src/services/auth.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 
 const jwt = require("jsonwebtoken");
 
-const { JWT_SECRET, JWT_TIMING } = process.env;
+const { JWT_SECRET, JWT_TIMING, REFRESH_TOKEN_SECRET } = process.env;
 
 const saltRounds = bcrypt.genSaltSync(10);
 
@@ -19,19 +19,42 @@ const hashPassword = async (req, res, next) => {
   }
 };
 
+const generateAccessToken = (userId) => {
+  return (
+    jwt.sign({ sub: userId }, JWT_SECRET),
+    {
+      expiresIn: JWT_TIMING,
+    }
+  );
+};
+
+const generateRefreshToken = (userId) => {
+  return (
+    jwt.sign({ sub: userId }, JWT_SECRET),
+    {
+      expiresIn: JWT_TIMING,
+    }
+  );
+};
+
 const verifyPassword = async (req, res) => {
   const hash = req.user.hashedPassword;
   const { password } = req.body;
   try {
     const match = await bcrypt.compare(password, hash);
     if (match) {
-      const token = jwt.sign({ sub: req.user.id }, JWT_SECRET, {
-        expiresIn: JWT_TIMING,
-      });
-      delete req.user.hashedPassword;
-      delete req.body.password;
+      const accessToken = generateAccessToken(req.user.id);
+      const refreshToken = generateRefreshToken(req.user.id);
+
+      delete req.user.hashPassword;
+      delete req.user.password;
+
       res
-        .cookie("accesstoken", token, {
+        .cookie("accesstoken", accessToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+        })
+        .cookie("refreshToken", refreshToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
         })
@@ -42,6 +65,14 @@ const verifyPassword = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.sendStatus(400);
+  }
+};
+const verifyRefreshToken = (refreshToken) => {
+  try {
+    const payload = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+    return payload.sub;
+  } catch (error) {
+    return null;
   }
 };
 
@@ -62,4 +93,10 @@ const logout = (req, res) => {
   res.status(200).json("User has been logged out");
 };
 
-module.exports = { hashPassword, verifyPassword, verifyToken, logout };
+module.exports = {
+  hashPassword,
+  verifyPassword,
+  verifyToken,
+  logout,
+  verifyRefreshToken,
+};
