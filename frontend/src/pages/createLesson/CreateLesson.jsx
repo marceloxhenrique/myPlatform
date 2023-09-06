4385c6b3dae425e832be72ad9a30b9a2ec42aeb6
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-09-06 22:10:49 +0200

    Updated ComponenetCreateLesson.jsx

--- frontend/src/pages/createLesson/CreateLesson.jsx
+++ frontend/src/pages/createLesson/CreateLesson.jsx
@@ -5,7 +5,6 @@ import styles from "./CreateLesson.module.css";
 export default function CreateLesson() {
   return (
     <div className={styles.containerCreatelesson}>
-      <h1>Create a New lesson</h1>
       <ComponentCreateLesson />
     </div>
   );
